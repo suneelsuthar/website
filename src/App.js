d8b3f4cbc3d50051c5d9e62c83e96c0868d928f8
suneelsuthar <suneelsuther123@gmail.com>
2020-01-04 20:04:28 +0500

    change heading

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>Hello World</h1>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
         <a
           className="App-link"
           href="https://reactjs.org"
